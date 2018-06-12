9aea912d41c561b3262b4e82eaad2e913c7dd0ac
NakanishiJun <j.nakanishi@gingerapp.co.jp>
2018-06-12 18:53:05 +0900

    fixed

--- bh_sshcmd.py
+++ bh_sshcmd.py
@@ -1,13 +1,13 @@
 # -*- coding:utf-8 -*-
 
 import threading 
-import paramikio
+import paramiko
 import subprocess
 
 def ssh_command(ip, user, passwd, command):
-	client paramikio.SSHClient()
+	client = paramiko.SSHClient()
 	#client.load_host_keys('/home/justin/.ssh/known_hosts')
-	client.set_missing_host_key_oplicy(paramikio.AutoAddPolicy())
+	client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 	client.connect(ip, username=user, password=passwd)
 	ssh_session = client.get_transport().open_session()
 	if ssh_session.active:
@@ -15,4 +15,4 @@ def ssh_command(ip, user, passwd, command):
 		print ssh_session.recv(4096)
 	return
 
-ssh_command('192.168.223.136', 'justin', 'lovesthepython', 'id')
+ssh_command('192.168.56.100', 'justin', 'lovesthepython', 'id')
